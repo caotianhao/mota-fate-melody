c3c6dea902f0b8deafdf9da4d0e91b03ab80a6f9
caotianhao <caotianhao@naver.com>
2026-02-21 23:22:52 +0800

    feat: fate1 - melody start

---
+++ project/floors/MT0.js
@@ -0,0 +1,52 @@
+main.floors.MT0=
+{
+    "floorId": "MT0",
+    "title": "主塔 0 层",
+    "name": "0",
+    "canFlyTo": true,
+    "canFlyFrom": true,
+    "canUseQuickShop": true,
+    "cannotViewMap": false,
+    "defaultGround": "ground",
+    "images": [],
+    "ratio": 1,
+    "map": [
+    [  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2],
+    [  2,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  2],
+    [  2,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  2],
+    [  2,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  2],
+    [  2,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  2],
+    [  2,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  2],
+    [  2,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  2],
+    [  2,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  2],
+    [  2,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  2],
+    [  2,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  2],
+    [  2,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  2],
+    [  2,  0,  0,  0,  0,  0, 87,  0,  0,  0,  0,  0,  2],
+    [  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2]
+],
+    "firstArrive": [],
+    "parallelDo": "",
+    "events": {},
+    "changeFloor": {
+        "6,11": {
+            "floorId": ":next",
+            "stair": "downFloor"
+        }
+    },
+    "afterBattle": {},
+    "afterGetItem": {},
+    "afterOpenDoor": {},
+    "cannotMove": {},
+    "bgmap": [
+
+],
+    "fgmap": [
+
+],
+    "width": 13,
+    "height": 13,
+    "autoEvent": {},
+    "beforeBattle": {},
+    "cannotMoveIn": {}
+}
---
+++ project/floors/MT1.js
@@ -0,0 +1,57 @@
+main.floors.MT1=
+{
+    "floorId": "MT1",
+    "title": "主塔 1 层",
+    "name": "1",
+    "width": 13,
+    "height": 13,
+    "canFlyTo": true,
+    "canFlyFrom": true,
+    "canUseQuickShop": true,
+    "cannotViewMap": false,
+    "images": [],
+    "ratio": 1,
+    "defaultGround": "ground",
+    "firstArrive": [],
+    "eachArrive": [],
+    "parallelDo": "",
+    "events": {},
+    "changeFloor": {
+        "7,6": {
+            "floorId": ":next",
+            "stair": "downFloor"
+        },
+        "6,12": {
+            "floorId": ":before",
+            "stair": "upFloor"
+        }
+    },
+    "beforeBattle": {},
+    "afterBattle": {},
+    "afterGetItem": {},
+    "afterOpenDoor": {},
+    "autoEvent": {},
+    "cannotMove": {},
+    "cannotMoveIn": {},
+    "map": [
+    [  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],
+    [  1, 22, 23, 21,  1,203,202,201,  1, 32,  0,123,  1],
+    [  1, 27, 27, 28,  1, 35,  1, 85,  1, 32,  0,  0,  1],
+    [  1, 32, 32, 32,  1,  1,122,  0,  1,  1, 86,  1,  1],
+    [  1,  1, 83,  1,  1,  0,  0,  0,209,210,  0, 32,  1],
+    [  1, 27, 21,201,202,205,  1,  1,  1,  1, 82,  1,  1],
+    [  1,  1, 81,  1,  1,  0,  1, 87,  0,202,  0, 28,  1],
+    [  1, 31,201,  0,  1, 27,  1,  0,  0,  1,201,  0,  1],
+    [  1,210,  1,  0,202, 21,  1,  0,201, 81,  0, 32,  1],
+    [  1,  0, 81,  0,  1, 81,  1, 82,  1,  1, 81,  1,  1],
+    [  1, 28,  1,  0,  1,  0,  0,  0,201, 21, 22,121,  1],
+    [  1, 22,  0,  0, 81,124,  0,  0,  1, 45, 51, 27,  1],
+    [  1,  1,  1,  1,  1,  1, 88,  1,  1,  1,  1,  1,  1]
+],
+    "bgmap": [
+
+],
+    "fgmap": [
+
+]
+}
