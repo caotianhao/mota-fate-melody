ccf6848295fedd2672636119bdab80f33751a01f
caotianhao <caotianhao@naver.com>
2026-03-01 16:35:26 +0800

    feat: update MT0 floor details, change ground type to grass, modify map layout, and enhance event dialogues with new interactions

--- project/data.js
+++ project/data.js
@@ -239,9 +239,9 @@ var data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d =
 				"equips": {}
 			},
 			"loc": {
-				"direction": "up",
+				"direction": "down",
 				"x": 6,
-				"y": 10
+				"y": 3
 			},
 			"flags": {},
 			"followers": [],
