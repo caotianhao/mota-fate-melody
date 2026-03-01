f3cab857b96921aa826fa3068e7b9fa565b2184a
caotianhao <caotianhao@naver.com>
2026-03-01 13:52:12 +0800

    feat: update MT16, MT18, and MT19 floor details, adjust event dialogues, and enhance floor transitions

--- project/floors/MT18.js
+++ project/floors/MT18.js
@@ -20,7 +20,7 @@ main.floors.MT18=
             "\t[普通老人,man]在魔塔里，史莱姆的确是最低等级的魔物，但是有一种史莱姆是个例外",
             "\t[普通老人,man]它就是疫病化身，吸收了无数怨念与疫病，身体结构发生了改变，几乎一触碰就会染上各种致命的传染病",
             "\t[普通老人,man]最基础的解毒剂是无法解除跟这种魔物战斗以后的后遗症的",
-            "\t[普通老人,man]我听说这个区域的隐藏空间里有这样一种灵药，能够根治白冰，或许能够派上用场",
+            "\t[普通老人,man]我听说这个区域的隐藏空间里有这样一种灵药，能够根治百病，或许能够派上用场",
             {
                 "type": "hide",
                 "remove": true,
@@ -39,10 +39,164 @@ main.floors.MT18=
         }
     },
     "beforeBattle": {},
-    "afterBattle": {},
+    "afterBattle": {
+        "7,2": [
+            {
+                "type": "setValue",
+                "name": "flag:door_MT18_6_4",
+                "operator": "+=",
+                "value": "1"
+            }
+        ],
+        "5,2": [
+            {
+                "type": "setValue",
+                "name": "flag:door_MT18_6_4",
+                "operator": "+=",
+                "value": "1"
+            }
+        ],
+        "5,5": [
+            {
+                "type": "setValue",
+                "name": "flag:door_MT18_6_6",
+                "operator": "+=",
+                "value": "1"
+            }
+        ],
+        "7,5": [
+            {
+                "type": "setValue",
+                "name": "flag:door_MT18_6_6",
+                "operator": "+=",
+                "value": "1"
+            }
+        ],
+        "6,8": [
+            {
+                "type": "setValue",
+                "name": "flag:door_MT18_6_10",
+                "operator": "+=",
+                "value": "1"
+            },
+            {
+                "type": "setValue",
+                "name": "flag:door_MT18_3_8",
+                "operator": "+=",
+                "value": "1"
+            },
+            {
+                "type": "setValue",
+                "name": "flag:door_MT18_9_8",
+                "operator": "+=",
+                "value": "1"
+            }
+        ]
+    },
     "afterGetItem": {},
     "afterOpenDoor": {},
-    "autoEvent": {},
+    "autoEvent": {
+        "6,4": {
+            "0": {
+                "condition": "flag:door_MT18_6_4==2",
+                "currentFloor": true,
+                "priority": 0,
+                "delayExecute": false,
+                "multiExecute": false,
+                "data": [
+                    {
+                        "type": "openDoor"
+                    },
+                    {
+                        "type": "setValue",
+                        "name": "flag:door_MT18_6_4",
+                        "operator": "=",
+                        "value": "null"
+                    }
+                ]
+            }
+        },
+        "6,6": {
+            "0": {
+                "condition": "flag:door_MT18_6_6==2",
+                "currentFloor": true,
+                "priority": 0,
+                "delayExecute": false,
+                "multiExecute": false,
+                "data": [
+                    {
+                        "type": "openDoor"
+                    },
+                    {
+                        "type": "setValue",
+                        "name": "flag:door_MT18_6_6",
+                        "operator": "=",
+                        "value": "null"
+                    }
+                ]
+            }
+        },
+        "6,10": {
+            "0": {
+                "condition": "flag:door_MT18_6_10==1",
+                "currentFloor": true,
+                "priority": 0,
+                "delayExecute": false,
+                "multiExecute": false,
+                "data": [
+                    {
+                        "type": "openDoor"
+                    },
+                    {
+                        "type": "setValue",
+                        "name": "flag:door_MT18_6_10",
+                        "operator": "=",
+                        "value": "null"
+                    }
+                ]
+            }
+        },
+        "3,8": {
+            "0": {
+                "condition": "flag:door_MT18_3_8==1",
+                "currentFloor": true,
+                "priority": 0,
+                "delayExecute": false,
+                "multiExecute": false,
+                "data": [
+                    {
+                        "type": "openDoor"
+                    },
+                    {
+                        "type": "setValue",
+                        "name": "flag:door_MT18_3_8",
+                        "operator": "=",
+                        "value": "null"
+                    }
+                ]
+            }
+        },
+        "9,8": {
+            "0": {
+                "condition": "flag:door_MT18_9_8==1",
+                "currentFloor": true,
+                "priority": 0,
+                "delayExecute": false,
+                "multiExecute": false,
+                "data": [
+                    {
+                        "type": "openDoor"
+                    },
+                    {
+                        "type": "setValue",
+                        "name": "flag:door_MT18_9_8",
+                        "operator": "=",
+                        "value": "null"
+                    }
+                ]
+            }
+        }
+    },
     "cannotMove": {},
     "cannotMoveIn": {},
     "map": [
@@ -50,14 +204,14 @@ main.floors.MT18=
     [  1,1276,  1,208, 81,  1, 33,  1,  1,208, 82, 88,  1],
     [  1, 34, 82,1276,  0,214,121,214, 81,1283,  0,  0,  1],
     [  1,1283,  1,208,  1,  1,  0,  1,  1,208,  1,  1,  1],
-    [  1,  1,1276,  1,1276,  1, 85,  1,1283,  1,1283,  1,  1],
-    [  1, 21,213,  0,  0,  0,  0,  0,  0,  0,213, 21,  1],
-    [  1,  1,  0,  1,  0,  0,  1,  0,  0,  1,  0,  1,  1],
-    [  1, 34,213,  0,  0,  0,  0,  0,  0,  0,213, 34,  1],
-    [  1,  1,  0,  1,  0,  0,  1,  0,  0,  1,  0,  1,  1],
-    [  1, 28,213,  0,  0,  0,  0,  0,  0,  0,213, 27,  1],
-    [  1,  1,  0,  1,  0,  1,248,  1,  0,  1,  0,  1,  1],
-    [  1, 28,  0,214,  0,  1,  0,  1,  0,214,  0, 27,  1],
+    [  1,  1,  1,  1,  1,  1, 85,  1,  1,  1,  1,  1,  1],
+    [  1,  5,  5,  5,  1,214,  0,214,  1,  5,  5,  5,  1],
+    [  1,  1,  1,  1,  1,  1, 85,  1,  1,  1,  1,  1,  1],
+    [  1, 28,1283,  1,  0,  0,  0,  0,  0,  1,1276, 27,  1],
+    [  1, 34, 21, 85,  0,  0,248,  0,  0, 85, 21, 34,  1],
+    [  1, 28,1283,  1,  0,  0,  0,  0,  0,  1,1276, 27,  1],
+    [  1,  1,  1,  1,  1,  1, 85,  1,  1,  1,  1,  1,  1],
+    [  1,  5,  5,  5,  5,  1,  0,  1,  5,  5,  5,  5,  1],
     [  1,  1,  1,  1,  1,  1, 87,  1,  1,  1,  1,  1,  1]
 ],
     "bgmap": [
--- project/floors/MT19.js
+++ project/floors/MT19.js
@@ -1,42 +1,231 @@
 main.floors.MT19=
 {
-"floorId": "MT19",
-"title": "主塔 19 层",
-"name": "19",
-"width": 13,
-"height": 13,
-"canFlyTo": true,
-"canFlyFrom": true,
-"canUseQuickShop": true,
-"cannotViewMap": false,
-"images": [],
-"ratio": 1,
-"defaultGround": "ground",
-"firstArrive": [],
-"eachArrive": [],
-"parallelDo": "",
-"events": {},
-"changeFloor": {},
-"beforeBattle": {},
-"afterBattle": {},
-"afterGetItem": {},
-"afterOpenDoor": {},
-"autoEvent": {},
-"cannotMove": {},
-"cannotMoveIn": {},
-"map": [
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]
+    "floorId": "MT19",
+    "title": "主塔 19 层",
+    "name": "19",
+    "width": 13,
+    "height": 13,
+    "canFlyTo": true,
+    "canFlyFrom": true,
+    "canUseQuickShop": true,
+    "cannotViewMap": false,
+    "images": [],
+    "ratio": 1,
+    "defaultGround": "ground",
+    "firstArrive": [],
+    "eachArrive": [],
+    "parallelDo": "",
+    "events": {
+        "5,11": [
+            "\t[盗贼,thief]嘿！哥们儿！我们可真是有缘分啊！",
+            "\t[勇者]啊，好久不见。这个区域的收成如何？",
+            "\t[盗贼,thief]嘿嘿嘿嘿~~发达了发达了，不愧是鬼帝镇守的区域啊，好宝贝就是多！",
+            "\t[勇者]这么说，你认识鬼帝咯？",
+            "\t[盗贼,thief]这......鬼帝的名字，只要对魔塔文化有涉猎的人一般都会知道的吧？不用在意这些细节。",
+            "\t[盗贼,thief]你一路战斗到这里，一定很吃力吧？",
+            "\t[盗贼,thief]作为我们友谊的证明，我可以帮你打开一扇这层的宝物牢门！这样你的能力就能得到大幅提升了！",
+            {
+                "type": "openDoor",
+                "loc": [
+                    4,
+                    10
+                ]
+            },
+            "\t[勇者]竟然有这等好事？那可真是谢谢你了啊！",
+            "\t[盗贼,thief]嘿嘿嘿。小事小事，比起这个，你赶快战胜那个虚有其表的家伙，前往下一个区域吧！",
+            "\t[盗贼,thief]更高层的区域可是有着数不清的珍宝啊！",
+            "\t[勇者]啊，我尽力吧......不过对手可是鬼帝这种级别的存在，这或许是我们最后一次见面也说不定哦。",
+            "\t[盗贼,thief]没有的事！你一定能赢的！",
+            "\t[盗贼,thief]就这样，我去挖密道了！我们第三区域再见！你也赶紧前进吧！",
+            {
+                "type": "hide",
+                "remove": true,
+                "time": 500
+            }
+        ]
+    },
+    "changeFloor": {
+        "6,0": {
+            "floorId": ":next",
+            "stair": "downFloor"
+        },
+        "6,12": {
+            "floorId": ":before",
+            "stair": "upFloor"
+        }
+    },
+    "beforeBattle": {},
+    "afterBattle": {
+        "5,9": [
+            {
+                "type": "setValue",
+                "name": "flag:door_MT19_6_8",
+                "operator": "+=",
+                "value": "1"
+            }
+        ],
+        "7,9": [
+            {
+                "type": "setValue",
+                "name": "flag:door_MT19_6_8",
+                "operator": "+=",
+                "value": "1"
+            }
+        ],
+        "5,7": [
+            {
+                "type": "setValue",
+                "name": "flag:door_MT19_6_6",
+                "operator": "+=",
+                "value": "1"
+            }
+        ],
+        "7,7": [
+            {
+                "type": "setValue",
+                "name": "flag:door_MT19_6_6",
+                "operator": "+=",
+                "value": "1"
+            }
+        ],
+        "5,5": [
+            {
+                "type": "setValue",
+                "name": "flag:door_MT19_6_4",
+                "operator": "+=",
+                "value": "1"
+            }
+        ],
+        "7,5": [
+            {
+                "type": "setValue",
+                "name": "flag:door_MT19_6_4",
+                "operator": "+=",
+                "value": "1"
+            }
+        ],
+        "5,3": [
+            {
+                "type": "setValue",
+                "name": "flag:door_MT19_6_2",
+                "operator": "+=",
+                "value": "1"
+            }
+        ],
+        "7,3": [
+            {
+                "type": "setValue",
+                "name": "flag:door_MT19_6_2",
+                "operator": "+=",
+                "value": "1"
+            }
+        ]
+    },
+    "afterGetItem": {},
+    "afterOpenDoor": {},
+    "autoEvent": {
+        "6,8": {
+            "0": {
+                "condition": "flag:door_MT19_6_8==2",
+                "currentFloor": true,
+                "priority": 0,
+                "delayExecute": false,
+                "multiExecute": false,
+                "data": [
+                    {
+                        "type": "openDoor"
+                    },
+                    {
+                        "type": "setValue",
+                        "name": "flag:door_MT19_6_8",
+                        "operator": "=",
+                        "value": "null"
+                    }
+                ]
+            }
+        },
+        "6,6": {
+            "0": {
+                "condition": "flag:door_MT19_6_6==2",
+                "currentFloor": true,
+                "priority": 0,
+                "delayExecute": false,
+                "multiExecute": false,
+                "data": [
+                    {
+                        "type": "openDoor"
+                    },
+                    {
+                        "type": "setValue",
+                        "name": "flag:door_MT19_6_6",
+                        "operator": "=",
+                        "value": "null"
+                    }
+                ]
+            }
+        },
+        "6,4": {
+            "0": {
+                "condition": "flag:door_MT19_6_4==2",
+                "currentFloor": true,
+                "priority": 0,
+                "delayExecute": false,
+                "multiExecute": false,
+                "data": [
+                    {
+                        "type": "openDoor"
+                    },
+                    {
+                        "type": "setValue",
+                        "name": "flag:door_MT19_6_4",
+                        "operator": "=",
+                        "value": "null"
+                    }
+                ]
+            }
+        },
+        "6,2": {
+            "0": {
+                "condition": "flag:door_MT19_6_2==2",
+                "currentFloor": true,
+                "priority": 0,
+                "delayExecute": false,
+                "multiExecute": false,
+                "data": [
+                    {
+                        "type": "openDoor"
+                    },
+                    {
+                        "type": "setValue",
+                        "name": "flag:door_MT19_6_2",
+                        "operator": "=",
+                        "value": "null"
+                    }
+                ]
+            }
+        }
+    },
+    "cannotMove": {},
+    "cannotMoveIn": {},
+    "map": [
+    [  1,  1,  5,  5,  1,  1, 87,  1,  1,  5,  5,  1,  1],
+    [  1,  5,  5,  5,  5,  1,  0,  1,  5,  5,  5,  5,  1],
+    [  1,  5,  5,  5,  5,  1, 85,  1,  5,  5,  5,  5,  1],
+    [  1,  5,  5,  5,  5,208,  0,208,  5,  5,  5,  5,  1],
+    [  1,  5,  5,  5,  5,  1, 85,  1,  5,  5,  5,  5,  1],
+    [  1,  5,  5,  5,  5,248,  0,248,  5,  5,  5,  5,  1],
+    [  1,  5,  5,  5,  5,  1, 85,  1,  5,  5,  5,  5,  1],
+    [  1,  5,  5,  5,  1,384,  0,384,  1,  5,  5,  5,  1],
+    [  1,  1,  1,  1,  1,  1, 85,  1,  1,  1,  1,  1,  1],
+    [  1, 34, 28, 27,  1,214,  0,214,  1,1276,1283, 33,  1],
+    [  1, 34, 28, 27, 85,  0,  0,  0, 85,1276,1283, 33,  1],
+    [  1, 34, 28, 27,  1,123,  0,  0,  1,1276,1283, 33,  1],
+    [  1,  1,  1,  1,  1,  1, 88,  1,  1,  1,  1,  1,  1]
 ],
+    "bgmap": [
+
+],
+    "fgmap": [
+
+]
 }
