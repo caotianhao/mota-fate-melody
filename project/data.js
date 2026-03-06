c50687278dce3eaed2983185588fdd5b014659e5
caotianhao <caotianhao@naver.com>
2026-03-07 01:12:06 +0800

    fix: remove duplicate entry for floor EX1 and ensure correct placement in data structure

--- project/data.js
+++ project/data.js
@@ -18,7 +18,6 @@ var data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d =
 			"MT13",
 			"MT14",
 			"MT15",
-			"EX1",
 			"MT16",
 			"MT17",
 			"MT18",
@@ -53,7 +52,8 @@ var data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d =
 			"MT47",
 			"MT48",
 			"MT49",
-			"MT50"
+			"MT50",
+			"EX1"
 		],
 		"floorPartitions": [],
 		"images": [
