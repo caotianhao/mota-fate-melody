40d595e51dea4a6eb5fdb9ad019a643d36e2c648
caotianhao <caotianhao@naver.com>
2026-03-28 11:09:50 +0800

    add skeletonknight npc for 10f

--- project/maps.js
+++ project/maps.js
@@ -127,6 +127,7 @@ var maps_90f36752_8815_4be8_b32b_d7fad1d0542e =
 	"278": {"cls":"enemys","id":"devilKnight"},
 	"279": {"cls":"enemys","id":"grayPriest"},
 	"300": {"cls":"terrains","id":"ground"},
+	"301": {"cls":"npcs","id":"N301"},
 	"305": {"cls":"terrains","id":"grass"},
 	"308": {"cls":"terrains","id":"grass2"},
 	"311": {"cls":"terrains","id":"ground2"},
