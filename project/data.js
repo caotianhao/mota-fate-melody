e1cb257dfb1ccf56448e72662970242be4aea689
caotianhao <caotianhao@naver.com>
2026-03-28 12:18:16 +0800

    use fly must near the stair

--- project/data.js
+++ project/data.js
@@ -713,7 +713,7 @@ var data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d =
 		],
 		"autoScale": true,
 		"extendToolbar": false,
-		"flyNearStair": false,
+		"flyNearStair": true,
 		"flyRecordPosition": false,
 		"itemFirstText": true,
 		"equipboxButton": false,
